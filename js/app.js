a2af4fd6c0dc2201f37fb166cf360a662c11a621
Ionela Pastrama <ionipas@yahoo.com>
2018-07-26 23:16:16 +0300

    set the start position for enemy on X axes

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 var Enemy = function(x, y, speed, sprite) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    this.x = x;
+    this.x = - 100;
     this.y = y;
     this.speed = random(20, 70);
     // The image/sprite for our enemies, this uses
